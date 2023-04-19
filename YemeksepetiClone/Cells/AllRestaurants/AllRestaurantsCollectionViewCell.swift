b5f53bae4580281f0d9092e1fa0882dd20c9afa1
Batuhan Demircioğlu <bthn_chn_dmrc@hotmail.com>
2023-04-19 16:18:37 +0300

    project completed

--- YemeksepetiClone/Cells/AllRestaurants/AllRestaurantsCollectionViewCell.swift
+++ YemeksepetiClone/Cells/AllRestaurants/AllRestaurantsCollectionViewCell.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class AllRestaurantsCollectionViewCell: UICollectionViewCell {
 
+
+    @IBOutlet weak var delvieryPrice: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    static let identifier = String(describing: AllRestaurantsCollectionViewCell.self)
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
+    
+    func setup(restaurant: Restaurant) {
+        nameLabel.text = restaurant.title
+        categoryLabel.text = restaurant.category
+        delvieryPrice.text = restaurant.servicePrice
+        imageView.image = UIImage(named: restaurant.image)
+    }
 
 }
+
+
--- YemeksepetiClone/SecondViewController.swift
+++ YemeksepetiClone/SecondViewController.swift
@@ -10,26 +10,57 @@ import UIKit
 class SecondViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var scrollView: UIScrollView!
     
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var allRestaurantsCollectionView: UICollectionView!
     @IBOutlet weak var offersCollectionView: UICollectionView!
     @IBOutlet weak var searchBar: UISearchBar!
     
     
+    
+    
     var offers: [Offer] = [Offer(image: "offer1"),Offer(image: "offer2"),Offer(image: "offer3"),Offer(image: "offer4"),Offer(image: "offer5"),Offer(image: "offer6"),Offer(image: "offer2"),Offer(image: "offer1"),Offer(image: "offer2")]
+    
+    
+    var restaurants: [Restaurant] = [Restaurant(title: "Urfalı Hacıoğlu İbrahim Halil Yaşar", category: "Kebap & Türk Mutfağı, Restoran Teslimatlı", servicePrice: "50,00 TL min. sepet tutarı -", image: "rest1"),Restaurant(title: "Urfalı Hacıoğlu İbrahim Halil Yaşar", category: "Kebap & Türk Mutfağı, Restoran Teslimatlı", servicePrice: "50,00 TL min. sepet tutarı -", image: "rest1"),Restaurant(title: "Urfalı Hacıoğlu İbrahim Halil Yaşar", category: "Kebap & Türk Mutfağı, Restoran Teslimatlı", servicePrice: "50,00 TL min. sepet tutarı -", image: "rest1"),Restaurant(title: "Urfalı Hacıoğlu İbrahim Halil Yaşar", category: "Kebap & Türk Mutfağı, Restoran Teslimatlı", servicePrice: "50,00 TL min. sepet tutarı -", image: "rest1"),Restaurant(title: "Urfalı Hacıoğlu İbrahim Halil Yaşar", category: "Kebap & Türk Mutfağı, Restoran Teslimatlı", servicePrice: "50,00 TL min. sepet tutarı -", image: "rest1"),Restaurant(title: "Urfalı Hacıoğlu İbrahim Halil Yaşar", category: "Kebap & Türk Mutfağı, Restoran Teslimatlı", servicePrice: "50,00 TL min. sepet tutarı -", image: "rest1"),Restaurant(title: "Urfalı Hacıoğlu İbrahim Halil Yaşar", category: "Kebap & Türk Mutfağı, Restoran Teslimatlı", servicePrice: "50,00 TL min. sepet tutarı -", image: "rest1")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
-        offersCollectionView.showsHorizontalScrollIndicator = false
+
+        // Disable scrolling on the collection view
+
+        // Calculate the height of the collection view based on the cell content size
+        let collectionViewHeight = allRestaurantsCollectionView.collectionViewLayout.collectionViewContentSize.height
+
+        // Set the height of the view containing the collection view to match the height of the collection view
+        contentView.frame.size.height = collectionViewHeight
+
+        // Calculate the combined height of the view containing the collection view and any other components above it
+        let combinedHeight = contentView.frame.origin.y + contentView.frame.size.height
+
+        // Set the height of the content view to match the combined height
+        contentView.frame.size.height = combinedHeight
+
+        // Set the content size of the scroll view to fit the height of the content view
+        scrollView.contentSize = contentView.frame.size
+
+
         
+        offersCollectionView.showsHorizontalScrollIndicator = false
         offersCollectionView.dataSource = self
         offersCollectionView.delegate = self
+        
+        allRestaurantsCollectionView.showsHorizontalScrollIndicator = false
+        allRestaurantsCollectionView.dataSource = self
+        allRestaurantsCollectionView.delegate = self
         
         setupSearchBar()
         scrollView.delegate = self
         scrollView.contentInsetAdjustmentBehavior = .never
         
         offersCollectionView.register(UINib(nibName: OffersCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: OffersCollectionViewCell.identifier)
+        allRestaurantsCollectionView.register(UINib(nibName: AllRestaurantsCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: AllRestaurantsCollectionViewCell.identifier)
         
         
         // Do any additional setup after loading the view.
@@ -62,14 +93,34 @@ class SecondViewController: UIViewController, UIScrollViewDelegate {
 
 extension SecondViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return offers.count
+        switch collectionView {
+        case offersCollectionView:
+            return offers.count
+        case allRestaurantsCollectionView:
+            return restaurants.count
+        default: return 0
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OffersCollectionViewCell.identifier, for: indexPath) as! OffersCollectionViewCell
-        cell.setup(offer: offers[indexPath.row])
-        return cell
+        
+        
+        switch collectionView {
+        case offersCollectionView:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OffersCollectionViewCell.identifier, for: indexPath) as! OffersCollectionViewCell
+            cell.setup(offer: offers[indexPath.row])
+            return cell
+        case allRestaurantsCollectionView:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AllRestaurantsCollectionViewCell.identifier, for: indexPath) as! AllRestaurantsCollectionViewCell
+            cell.setup(restaurant: restaurants[indexPath.row])
+            return cell
+        default: return UICollectionViewCell()
+        
+        
     }
     
     
+}
+
+
 }
